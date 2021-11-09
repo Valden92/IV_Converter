aec5903346b802d9b999df3f4451315c614d7d27
lord_of_reality <83411504+Valden92@users.noreply.github.com>
2021-11-09 18:23:48 +0700

    Преобразовал функции в статические методы класса

--- Window/analitycal_class.py
+++ Window/analitycal_class.py
@@ -5,109 +5,6 @@ import os
 from math import log, exp, pi
 
 
-def arr_separator(array):
-    """Разделяет массив на подмассивы, если в нем встречаются пробелы.
-    Дополнительно пересохраняет данные из str формата в float.
-
-    :param array - массив, в котором нужно выделить значения и разделить на подмассивы, если встречаются пробелы.
-             out - возвращаемый массив, если в array встречаются пробелы.
-    time_massive - массив, используемый для генерации подмассивов, если встречаются пробелы;
-                   в противном случае данный массив является возвращаемым значением функции.
-    """
-    out = []
-    time_missive = []
-    for value in range(len(array)):
-        if array[value].isspace():
-            if time_missive:
-                out.append(time_missive)
-                time_missive = []
-        else:
-            time_missive.append(float(array[value]))
-
-    # Если встречались пробелы, то массив out будет содержать подмассивы, иначе будет пустым.
-    if not out:
-        return time_missive
-    else:
-        return out
-
-
-def range_clipping(x, y):
-    """Отсекает в массивах данные, входящие определенный диапазон и возвращает эти данные.
-
-    Данные отсекаются в диапазоне Токов [10е-5 : 10е-4].
-    Проводится отсечение значений Токов (x) и по этим же индексам отсечение значений Напряжений (y).
-
-        i - начальный индекс для отсечения.
-        m - конечный индекс для отсечения.
-    new_x - усеченный массив токов, привиденных к натуральному логарифму.
-    new_y - усеченный массив напряжений.
-    """
-    i, m = index_search(x)
-    new_x = list(map(log, x[i: m + 1]))
-    new_y = y[i: m + 1]
-
-    return new_x, new_y
-
-
-def index_search(x):
-    """Поиск индексов для отсечения данных.
-
-    Данные отсекаются в диапазоне Токов [10е-5 : 10е-4].
-    Осуществляется поиск крайних индексов, в которых значения массива Токов попадают в данный диапазон.
-
-    :param x - массив значений Токов.
-        i, m - возвращаемые значения начального и конечного индекса диапазона.
-    """
-    i = 0
-    m = 0
-
-    for v in range(len(x)):
-        if x[v] >= 1e-5:
-            i = v
-            break
-    for v in range(i, len(x)):
-        if x[v] >= 1e-4:
-            m = v
-            break
-
-    return i, m
-
-
-def search_b(x, y):
-    """
-    Поиск коэффициента b по методу наименьших квадратов (МНК).
-
-    :param  x - массив Напряжений.
-    :param  y - массив Токов.
-    :return m - возвращаемое экспоненциальное значение от найденного коэффициента.
-    """
-    x = numpy.array(x)
-    y = numpy.array(y)
-    massive = numpy.vstack([x, numpy.ones(len(x))]).T
-    k, m = numpy.linalg.lstsq(massive, y, rcond=None)[0]
-    m = exp(m)
-    return m
-
-
-def search_fi(bi, d):
-    """
-    Поиск коэффициента Фи.
-
-    :param bi - коэффициент найденный по методу МНК в функции search_b.
-    :param  d - диаметр контакта.
-    :return f - Возвращаемый коэффициент Фи.
-    """
-    # Объявление констант.
-    __K = 1.38e-23  # Постоянная Больцмана.
-    __A = 264         # Постоянная Ридчарсона.
-    __T = 300         # Температура нагрева во время измерения.
-    __Q = 1.6e-19   # Заряд
-
-    f = ((__K * __T) / __Q) * log(((__A * (__T ** 2)) / (bi / (((d ** 2) * pi) / 4))))
-
-    return f
-
-
 class AnalitycalCalc:
     """Получает файл и обрабатывает его особым образом.
     """
@@ -161,12 +58,12 @@ class AnalitycalCalc:
         except FileNotFoundError:
             raise FileNotFoundError('Файл не найден!')
         else:
-            self.direct_current = arr_separator(direct_current)
-            self.direct_voltage = arr_separator(direct_voltage)
+            self.direct_current = self.__arr_separator(direct_current)
+            self.direct_voltage = self.__arr_separator(direct_voltage)
 
             if self.is_inverse:
-                self.reverse_current = arr_separator(reverse_current)
-                self.reverse_voltage = arr_separator(reverse_voltage)
+                self.reverse_current = self.__arr_separator(reverse_current)
+                self.reverse_voltage = self.__arr_separator(reverse_voltage)
 
     def __filter_mass(self):
         """
@@ -193,10 +90,10 @@ class AnalitycalCalc:
 
     def __coefficient_calc(self):
         for i in range(len(self.direct_current)):
-            self.trunc_current[i], self.trunc_voltage[i] = range_clipping(self.direct_current[i],
-                                                                          self.direct_voltage[i])
-            self.b[i] = search_b(self.trunc_voltage[i], self.trunc_current[i])
-            self.fi[i] = search_fi(self.b[i], self.diameter)
+            self.trunc_current[i], self.trunc_voltage[i] = self.__range_clipping(self.direct_current[i],
+                                                                                 self.direct_voltage[i])
+            self.b[i] = self.__search_b(self.trunc_voltage[i], self.trunc_current[i])
+            self.fi[i] = self.__search_fi(self.b[i], self.diameter)
 
     def print_coefficient(self):
         if self.b:
@@ -210,3 +107,106 @@ class AnalitycalCalc:
         self.__read_file()
         self.__filter_mass()
         self.__coefficient_calc()
+        self.print_coefficient()
+
+    @staticmethod
+    def __arr_separator(array):
+        """Разделяет массив на подмассивы, если в нем встречаются пробелы.
+        Дополнительно пересохраняет данные из str формата в float.
+
+        :param array - массив, в котором нужно выделить значения и разделить на подмассивы, если встречаются пробелы.
+                 out - возвращаемый массив, если в array встречаются пробелы.
+        time_massive - массив, используемый для генерации подмассивов, если встречаются пробелы;
+                       в противном случае данный массив является возвращаемым значением функции.
+        """
+        out = []
+        time_missive = []
+        for value in range(len(array)):
+            if array[value].isspace():
+                if time_missive:
+                    out.append(time_missive)
+                    time_missive = []
+            else:
+                time_missive.append(float(array[value]))
+
+        # Если встречались пробелы, то массив out будет содержать подмассивы, иначе будет пустым.
+        if not out:
+            return time_missive
+        else:
+            return out
+
+    def __range_clipping(self, x, y):
+        """Отсекает в массивах данные, входящие определенный диапазон и возвращает эти данные.
+
+        Данные отсекаются в диапазоне Токов [10е-5 : 10е-4].
+        Проводится отсечение значений Токов (x) и по этим же индексам отсечение значений Напряжений (y).
+
+            i - начальный индекс для отсечения.
+            m - конечный индекс для отсечения.
+        new_x - усеченный массив токов, привиденных к натуральному логарифму.
+        new_y - усеченный массив напряжений.
+        """
+        i, m = self.__index_search(x)
+        new_x = list(map(log, x[i: m + 1]))
+        new_y = y[i: m + 1]
+
+        return new_x, new_y
+
+    @staticmethod
+    def __index_search(x):
+        """Поиск индексов для отсечения данных.
+
+        Данные отсекаются в диапазоне Токов [10е-5 : 10е-4].
+        Осуществляется поиск крайних индексов, в которых значения массива Токов попадают в данный диапазон.
+
+        :param x - массив значений Токов.
+            i, m - возвращаемые значения начального и конечного индекса диапазона.
+        """
+        i = 0
+        m = 0
+
+        for v in range(len(x)):
+            if x[v] >= 1e-5:
+                i = v
+                break
+        for v in range(i, len(x)):
+            if x[v] >= 1e-4:
+                m = v
+                break
+
+        return i, m
+
+    @staticmethod
+    def __search_b(x, y):
+        """
+        Поиск коэффициента b по методу наименьших квадратов (МНК).
+
+        :param  x - массив Напряжений.
+        :param  y - массив Токов.
+        :return m - возвращаемое экспоненциальное значение от найденного коэффициента.
+        """
+        x = numpy.array(x)
+        y = numpy.array(y)
+        massive = numpy.vstack([x, numpy.ones(len(x))]).T
+        k, m = numpy.linalg.lstsq(massive, y, rcond=None)[0]
+        m = exp(m)
+        return m
+
+    @staticmethod
+    def __search_fi(bi, d):
+        """
+        Поиск коэффициента Фи.
+
+        :param bi - коэффициент найденный по методу МНК в функции search_b.
+        :param  d - диаметр контакта.
+        :return f - Возвращаемый коэффициент Фи.
+        """
+        # Объявление констант.
+        __K = 1.38e-23  # Постоянная Больцмана.
+        __A = 264  # Постоянная Ридчарсона.
+        __T = 300  # Температура нагрева во время измерения.
+        __Q = 1.6e-19  # Заряд
+
+        f = ((__K * __T) / __Q) * log(((__A * (__T ** 2)) / (bi / (((d ** 2) * pi) / 4))))
+
+        return f
